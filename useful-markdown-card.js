c18032f6b1f6aa7debdf3143420264180d94c668
Thomas Lovén <thomasloven@gmail.com>
2018-08-15 23:49:30 +0200

    Rendering works, but not updating

--- useful-markdown-card.js
+++ useful-markdown-card.js
@@ -5,24 +5,43 @@ class UsefulMarkdownCard extends HTMLElement {
     this.md = document.createElement('hui-markdown-card');
 
     this.shadowRoot.appendChild(this.md);
+    this._hass = null;
+  }
+
+  handleTemplate(str) {
+    if(!this._hass) return '';
+    str = str.replace(/^\s+|\s+$/g, '');
+    let parts = str.split(".");
+    let v = this._hass.states[parts[0]+'.'+parts[1]];
+    parts.shift();
+    parts.shift();
+    parts.forEach(item => {
+      v = v[item];
+    });
+    return v;
   }
 
   process(text) {
-    text = text.replace(/\[\[(.*?)\]\]/g, 'match');
-    console.log(text);
+    text = text.replace(/\[\[(.*?)\]\]/g, (str,p1, offset,s) => this.handleTemplate(p1));
     return text;
   }
 
   setConfig(config) {
     this.title = config.title;
     this.content = config.content;
+    this._config = config;
 
     config.title = this.process(this.title);
     config.content = this.process(this.content);
-    this.md.setConfig(config);
+    this.md.setConfig(this._config);
   }
 
   set hass(hass) {
+    this._hass = hass;
+    this.md.hass = hass;
+    this._config.title = this.process(this.title);
+    this._config.content = this.process(this.content);
+    this.md.setConfig(this._config);
     this.md.hass = hass;
   }
 }
